5f01074ed26f7b7602c9ddcf9c89426e3b06aca4
Sam <Sam@Sam-MacBook.local>
2016-09-21 17:59:54 +0800

    add demo06

---
+++ demo06/entry.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component from './Compoent.jsx';
+
+require("./style.css");
+
+ReactDOM.render(<Component />,document.getElementById("app_container"));
