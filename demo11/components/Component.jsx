fc4ab43fffabf74ac5ae6032af97932a35cabfc5
Sam <Sam@Sam-MacBook.local>
2016-09-25 17:24:47 +0800

    add demo11

---
+++ demo11/components/Component.jsx
@@ -0,0 +1,20 @@
+
+export default class Component extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    var data = process.env.NODE_ENV;
+    return (
+      <div>
+        <div className="bg"/>
+        <div>
+          { "process.env.NODE_ENV的值是："+data }
+        </div>
+      </div>
+    )
+  }
+
+}
+
---
+++ demo11/server.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config');//webpack.config.js同一目录
+var port = 3666;
+var app = express();
+var compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,//必须跟webpack.config.js的ouput.publickPath一致
+}));
+//访问的静态文件
+app.use(express.static(path.join(__dirname, './public')));
+app.use(require('webpack-hot-middleware')(compiler));
+//所有请求都定位到index.html文件
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+var host = "localhost"
+app.listen(port, host, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.info("==> 🌎  Listening on port %s. Open up http://"+host+":%s/ in your browser.", port, port)
+});
