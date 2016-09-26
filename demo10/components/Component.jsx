177efd092524dd97565a9b721b04717816c650fb
Sam <Sam@Sam-MacBook.local>
2016-09-26 21:56:12 +0800

    test

--- demo10/components/Component.jsx
+++ demo10/components/Component.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Antd,{ Alert } from 'antd';
+
+require('antd/dist/antd.css')
 
 export default class Component extends React.Component {
   constructor(props){
@@ -12,6 +15,7 @@ export default class Component extends React.Component {
         <div className="bg"/>
         <div>
           { "process.env.NODE_ENV的值是："+data }
+          <Antd.Alert message="消息提示的文案" type="info" showIcon closable/>
         </div>
       </div>
     )
