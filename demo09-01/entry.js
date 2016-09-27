27a59a3bf1927dbb9171c6c027d3876cf86019c1
Sam <Sam@Sam-MacBook.local>
2016-09-27 20:22:07 +0800

    add demo09-01

---
+++ demo09-01/entry.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//没带后缀名
+import Component from 'com/Component';
+
+require("./style.css");
+
+ReactDOM.render(<Component />,document.getElementById("app_container"));
