45a883e3108005d4050bf2b66f4b4a4328c4897c
Sam <Sam@Sam-MacBook.local>
2016-09-28 11:02:43 +0800

    update demo10

--- demo10/entry.js
+++ demo10/entry.js
@@ -7,10 +7,11 @@ require("./style.css");
 
 ReactDOM.render(<Component />,document.getElementById("app_container"));
 
+//需要在这里加入代码，进行了整个app热替换
 if (module.hot) {
   module.hot.accept('com/Component', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
+    // 如果你使用了Webpack 2 in ES modules 模式, 你可以
+    // 直接使用 <App /> 而不用 require() 一个 <NextApp />.
     const NextApp = require('com/Component').default;
     ReactDOM.render(
       <NextApp />,
